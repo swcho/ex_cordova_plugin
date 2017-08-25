195477e87d7f0012aedb4099265607678242d427
Sungwoo <sungwoo.cho@navercorp.com>
2017-08-25 18:02:26 +0900

    Cordova Plugin Trial

---
+++ www/ModusEcho.js
@@ -0,0 +1,16 @@
+var argscheck = require('cordova/argscheck');
+var utils = require('cordova/utils');
+var exec = require('cordova/exec');
+var cordova = require('cordova');
+
+exports.echo = function (arg0, success, error) {
+    exec(success, error, "ModusEcho", "echo", [arg0]);
+};
+
+exports.echojs = function (arg0, success, error) {
+    if (arg0 && typeof (arg0) === 'string' && arg0.length > 0) {
+        success(arg0);
+    } else {
+        error('Empty message!');
+    }
+};
